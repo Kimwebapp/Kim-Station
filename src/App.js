adcfbc61238d969e2a91a84dbb414b65e81ace1d
Kimwebapp <comunicazioni@kimweb.it>
2025-06-13 21:13:33 +0000

    chore: aggiungi route /attivazioni per nuova pagina step by step React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Attivazioni from "./pages/Attivazioni";
 import Login from "./pages/Login";
 import ResetPassword from "./pages/ResetPassword";
 // import Assistenza from "./pages/Assistenza"; // Sblocca quando crei Assistenza.jsx
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+          <Route path="/attivazioni" element={<Attivazioni />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Account/Reset" element={<ResetPassword />} />
         {/* <Route path="/assistenza" element={<Assistenza />} /> */}
